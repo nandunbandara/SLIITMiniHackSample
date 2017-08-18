4632c15c36655a63620e9d15f592b20e3709ea10
Nandun Bandara <ntbandara3@hotmail.com>
2017-08-19 00:37:09 +0530

    login page with uc check

---
+++ src/pages/login/login.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams, LoadingController } from 'ionic-angular';
+
+
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+
+  username;
+  password;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public loadingCtrl: LoadingController) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+  login(){
+    //check user credentials
+    let username = this.username;
+    let password = this.password;
+
+    if(username == 'testUser' && password == 123){
+      let loader = this.loadingCtrl.create({
+        content: 'Logging in...',
+        duration: 2000
+      })
+      loader.present();
+    }else{
+    //  error : invalid credentials
+
+    }
+    // console.log('login function called');
+  }
+
+}
